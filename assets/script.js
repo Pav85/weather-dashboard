a32d7c5c2b6a623130e092733b7fb58a750fa828
Pav85 <pawel.werbowy@gmail.com>
2023-01-31 21:41:41 +0000

    set variables in js file, creates css file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+// console.log('hello world!');
+
+var APIkey = "11f628af2243468c36a25b569b68d689";
+
+var citySearch = $("#search-input");
+var searchBtn = $("#search-button");
+var history = $("#history");
+
+var displayHistoryCities = function() {
+   
+}
+
+
+
+
