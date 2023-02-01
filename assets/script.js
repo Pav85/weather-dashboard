bec1e5fd75b9dc72f6f021645f82e5cd7ad4c5f3
Pav85 <pawel.werbowy@gmail.com>
2023-02-01 12:02:17 +0000

    adds button to the js file

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,34 @@ var APIkey = "11f628af2243468c36a25b569b68d689";
 var citySearch = $("#search-input");
 var searchBtn = $("#search-button");
 var history = $("#history");
+var cityBtnArray = [];
 
-var displayHistoryCities = function() {
-   
+var renderCityButtons = function() {
+    
+    $("#history").empty();
+
+    for (var i = 0; i < cityBtnArray.length; i++) {
+        var a = $("<button>");
+
+        a.attr("history", cityBtnArray[i]);
+        a.attr("class", "btn btn-primary");
+        // a.attr("class", "btn btn-secondary");
+        a.text(cityBtnArray[i]);
+        $("#history").append(a);
+    }
+
 }
+
+searchBtn.on("click", function(event) {
+    event.preventDefault();
+
+    var city = $(searchBtn).val().trim();
+    cityBtnArray.push(city);
+
+    renderCityButtons();
+})
+
+
 
 
 
