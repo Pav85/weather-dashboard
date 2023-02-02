139e32aeb79696aff64497fdde4b0f5f7def0839
Pav85 <pawel.werbowy@gmail.com>
2023-02-02 21:27:34 +0000

    added button funcionality

--- assets/script.js
+++ assets/script.js
@@ -3,34 +3,54 @@
 var APIkey = "11f628af2243468c36a25b569b68d689";
 
 var citySearch = $("#search-input");
-var searchBtn = $("#search-button");
-var history = $("#history");
+// var searchBtn = $("#search-button");
+// var history = $("#history");
 var cityBtnArray = [];
 
-var renderCityButtons = function() {
+var city = $(this).attr("data-name");
+
+$("#search-button").addClass("btn btn-primary");
+
+function renderCityButtons() {
     
     $("#history").empty();
 
     for (var i = 0; i < cityBtnArray.length; i++) {
+        
         var a = $("<button>");
+    
+        a.addClass("btn btn-secondary buttonHistory");  
 
-        a.attr("history", cityBtnArray[i]);
-        a.attr("class", "btn btn-primary");
-        // a.attr("class", "btn btn-secondary");
+        // a.attr("city-button");
+
+        a.attr("data-name", cityBtnArray[i]);
+        // a.attr("class", "btn btn-primary");
+        // a.attr("class", "btn btn-secondary buttonHistory");
+        // a.text(cityBtnArray[i]);
         a.text(cityBtnArray[i]);
+
         $("#history").append(a);
     }
 
 }
 
-searchBtn.on("click", function(event) {
+// this function renders buttons after clikcking the search button
+
+$("#search-button").on("click", function(event) {
     event.preventDefault();
 
-    var city = $(searchBtn).val().trim();
+    var city = $("#search-input").val().trim();
+
+    citySearch.val("");
+
     cityBtnArray.push(city);
 
     renderCityButtons();
-})
+});
+
+// $(document).on("click", ".buttonHistory", displayWeather)
+
+renderCityButtons();
 
 
 
